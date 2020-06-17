c43a59918935a35d9d4d22c179f5d7cd0d6e9c9c
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-17 18:50:32 +0500

    Add fake data

---
+++ src/services/fakedata.js
@@ -0,0 +1,32 @@
+export const initialProducts =  [
+  {
+    id:1,
+    name: "CHECK PRINT SHIRT",
+    price: 110,
+    imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/M63H24W7JF0-L302-ALTGHOST?wid=1500&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
+  },
+  {
+    id:2,
+    name: "GLORIA HIGH LOGO SNEAKER",
+    price: 91,
+    imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/FLGLO4FAL12-BEIBR?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
+  },
+  {
+    id:3,
+    name: "CATE RIGID BAG",
+    price: 94.5,
+    imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/HWVG6216060-TAN?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
+  },
+  {
+    id:4,
+    name: "GUESS CONNECT WATCH",
+    price: 438.9,
+    imgUrl: "http://guesseu.scene7.com/is/image/GuessEU/WC0001FMSWC-G5?wid=520&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
+  },
+  {
+    id:5,
+    name: "'70s RETRO GLAM KEFIAH",
+    price: 20,
+    imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/AW6308VIS03-SAP?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
+  }
+]
