10f02379d86f085dbef69468f8fbc7f4baa832b0
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-23 16:17:34 +0500

    Update fakedata.js

--- src/services/fakedata.js
+++ src/services/fakedata.js
@@ -1,4 +1,4 @@
-export const initialProducts =  [
+export const dummyProducts =  [
   {
     id:1,
     name: "CHECK PRINT SHIRT",
@@ -29,4 +29,4 @@ export const initialProducts =  [
     price: 20,
     imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/AW6308VIS03-SAP?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
   }
-]
+];
