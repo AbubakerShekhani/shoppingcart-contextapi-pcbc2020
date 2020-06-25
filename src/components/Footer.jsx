a1e75d822b7e67f3ff0cb5857e9d0c77846c7919
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-25 17:04:59 +0500

    remove footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import styles from './Footer.module.scss';
 
 const Footer = () => {
     return (
-        <footer className={`${styles.footer}  mt-5 p-3`}>
+        <footer className="mt-5 p-3">
             2020 &copy; Abubaker Fashion Store.
 
             GitHub: <a href="https://github.com/Abubakershekhani">https://github.com/Abubakershekhani</a>
