5ed0879d99b5a19021158683b8489470f719ac56
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-27 15:45:07 +0500

    Update Header navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,9 @@ const Header = () => {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark mx-auto">
 
-        <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarColor01">
+        <div id="navbarColor01">
           <ul className="navbar-nav ">
 
             <li className="nav-item">
